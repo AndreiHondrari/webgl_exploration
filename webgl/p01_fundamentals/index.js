a2cd45dbb019d5da3c0c9680fc9eb2c3648f3132
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-06-17 21:05:14 +0300

    webGL rectangles example.

--- webgl/p01_fundamentals/index.js
+++ webgl/p01_fundamentals/index.js
@@ -9,25 +9,49 @@
         return;
     }
 
+    // function that handles the shader creation boilerplate
     function createShader(gl, type, source) {
         var shader = gl.createShader(type);
         gl.shaderSource(shader, source);
         gl.compileShader(shader);
         var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
         if (success) {
-        return shader;
+            return shader;
         }
 
         console.log(gl.getShaderInfoLog(shader));
         gl.deleteShader(shader);
     }
 
-    var vertexShaderSource = document.getElementById("2d-vertex-shader").text;
-    var fragmentShaderSource = document.getElementById("2d-fragment-shader").text;
+    const vertexShaderSource = `
+        // an attribute will receive data from a buffer
+        attribute vec4 a_position;
+
+        // all shaders have a main function
+        void main() {
+
+            // gl_Position is a special variable a vertex shader
+            // is responsible for setting
+            gl_Position = a_position;
+        }
+    `;
+
+    const fragmentShaderSource = `
+        // fragment shaders don't have a default precision so we need
+        // to pick one. mediump is a good default
+        precision mediump float;
+
+        void main() {
+            // gl_FragColor is a special variable a fragment shader
+            // is responsible for setting
+            gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
+        }
+    `;
 
     var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
     var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
+    // handle boilerplate program creation (vertex + fragment shaders)
     function createProgram(gl, vertexShader, fragmentShader) {
         var program = gl.createProgram();
         gl.attachShader(program, vertexShader);
@@ -42,8 +66,11 @@
         gl.deleteProgram(program);
     }
 
+    // make the program
     var program = createProgram(gl, vertexShader, fragmentShader);
     var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+
     var positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
@@ -51,16 +78,19 @@
     var positions = [
         0, 0,
         0, 0.5,
-        0.7, 0,
+        0.8, 0,
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    // * RENDERING *
+
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
     // Clear the canvas
     gl.clearColor(0, 0, 0, 0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // Tell it to use our program (pair of shaders)
+    // Tell WebGL to use our program (pair of shaders)
     gl.useProgram(program);
 
     gl.enableVertexAttribArray(positionAttributeLocation);
